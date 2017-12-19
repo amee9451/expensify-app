9d41d86519a5fc4df4bc0eec9f69f134978714b2
Ben Thompson <git@iambenthompson.com>
2017-12-19 12:33:09 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env', 'react'],
+                    plugins: [
+                        "transform-class-properties"
+                    ]
+                }
+            }],
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+        {
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ],
+            test: /\.s?css$/
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
