a8571493d34f7e497058be593e3e90350e5c240c
Ben Thompson <git@iambenthompson.com>
2017-12-21 09:23:00 -0500

    Implements basic Redux setup, includes obj spread babel plugin

--- src/app.js
+++ src/app.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({
+  description: 'Water bill',
+  amount: 4000,
+  createdAt: 0
+}));
+
+store.dispatch(addExpense({
+  description: 'Gas bill',
+  amount: 4300,
+  createdAt: 0
+}));
+
+store.subscribe(() => {
+  const state = store.getState();
+
+  console.log(getVisibleExpenses(state.expenses, state.filters));
+});
+
+store.dispatch(setTextFilter('bill'));
+
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
