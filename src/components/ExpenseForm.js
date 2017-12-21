41faa155b478a16df812fd47a6f0b1cf85f2361a
Ben Thompson <git@iambenthompson.com>
2017-12-21 12:37:47 -0500

    Adds ExpenseForm and wires it up to internally capture state of form

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: '',
+    amount: ''
+  };
+
+  onDescriptionChange = (event) => {
+    const description = event.target.value;
+    this.setState(() => ({ description }));
+  }
+
+  onNoteChange = (event) => {
+    const note = event.target.value;
+    this.setState(() => ({ note }));
+  }
+
+  onAmountChange = (event) => {
+    const amount = event.target.value;
+
+    if (amount.match(/^\d*(\.\d{0,2})?$/)){
+      this.setState(() => ({ amount }));
+    }
+  };
+
+  render () {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input 
+            type="text"
+            placeholder="Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default ExpenseForm;
