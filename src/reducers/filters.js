72e930704fc58ebe200a84b23ebdf49a98eb8ca1
Ben Thompson <git@iambenthompson.com>
2017-12-30 20:08:07 -0500

    Adds filters reducer test suite

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -13,31 +13,26 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
         ...state,
         text: action.text
       };
-      break;
     case 'SORT_BY_DATE':
       return {
         ...state,
         sortBy: 'date'
       };
-      break;
     case 'SORT_BY_AMOUNT':
       return {
         ...state,
         sortBy: 'amount'
       };
-      break;
     case 'SET_START_DATE':
       return {
         ...state,
         startDate: action.date
       };
-      break;
     case 'SET_END_DATE':
       return {
         ...state,
         endDate: action.date
       };
-      break;
     default:
       return state;
   }
