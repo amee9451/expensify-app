89abc4010f849b9a8f3babb59c6fa51fddcdbf54
Ben Thompson <git@iambenthompson.com>
2017-12-31 02:14:29 -0500

    Sets up basic webpack prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    app: './src/app.js'
-  },
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [{
-      use: [{
-        loader: 'babel-loader'
-      }],
-      test: /\.js$/,
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: {
+      app: './src/app.js'
     },
-    {
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ],
-      test: /\.s?css$/
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: '[name].bundle.js'
+    },
+    module: {
+      rules: [{
+        use: [{
+          loader: 'babel-loader'
+        }],
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        test: /\.s?css$/
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
