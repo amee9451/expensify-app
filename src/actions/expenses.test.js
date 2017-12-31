dc09e9eaf337510ddff7702912e905a43d692a64
Ben Thompson <git@iambenthompson.com>
2017-12-30 19:05:53 -0500

    Adds expenses actions test coverage

---
+++ src/actions/expenses.test.js
@@ -0,0 +1,56 @@
+import { addExpense, editExpense, removeExpense } from './expenses';
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  });
+});
+
+test('should setup edit expense action object', () => {
+  const action = editExpense('123456', { note: 'New note value' });
+
+  expect(action).toEqual({
+    id: '123456',
+    type: 'EDIT_EXPENSE',
+    updates: {
+      note: 'New note value'
+    }
+  });
+});
+
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent', 
+    note: 'This was last month\'s rent', 
+    amount: 109500, 
+    createdAt: 1000 
+  };
+
+  const action = addExpense(expenseData);
+
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test ('should setup add expense action object with default values', () => {
+  const action = addExpense();
+
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0,
+      id: expect.any(String)
+    }
+  });
+});
