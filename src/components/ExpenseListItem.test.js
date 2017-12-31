2696c651d5a5990890eb3f1ebef2b5b18ef2d824
Ben Thompson <git@iambenthompson.com>
2017-12-30 21:52:35 -0500

    Adds snapshot test suite for ExpenseListItem

---
+++ src/components/ExpenseListItem.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ExpenseListItem from './ExpenseListItem';
+
+import expenses from '../tests/fixtures/expenses';
+
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem { ...expenses[0] } />);
+
+  expect(wrapper).toMatchSnapshot();
+});
