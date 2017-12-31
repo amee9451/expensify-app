d11f216d289361c2967cac538836a439c13db8e2
Ben Thompson <git@iambenthompson.com>
2017-12-31 00:12:38 -0500

    Adds EditExpensePage test suite

---
+++ src/components/EditExpensePage.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { EditExpensePage } from './EditExpensePage';
+import ExpenseForm from './ExpenseForm';
+
+import expenses from '../tests/fixtures/expenses';
+
+let editExpense, removeExpense, expense, history, wrapper;
+beforeAll(() => {
+  editExpense = jest.fn();
+  removeExpense = jest.fn();
+  expense = expenses[0];
+  history = { push: jest.fn() };
+});
+
+beforeEach(() => {
+  wrapper = shallow(
+    <EditExpensePage 
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+      expense={expense}
+    />
+  );
+});
+
+test('should render EditExpensePage correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+  wrapper.find(ExpenseForm).prop('onSubmit')(expense);
+
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expense.id, expense);
+});
+
+test('should handle removeExpense', () => {
+  wrapper.find('button').simulate('click');
+
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({ id: expense.id });
+});
