c154119a2b900655f02f94e28bb782b452b24e2e
Ben Thompson <git@iambenthompson.com>
2018-01-02 12:00:51 -0500

    Adds set expenses logic and test suite to load expenses from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,9 +38,37 @@ const editExpense = (id, updates) => ({
   updates
 });
 
+const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+const startSetExpenses = () => {
+  return ((dispatch) => {
+    return database.ref('expenses')
+      .once('value')
+      .then((snapshot) => {
+        const expenses = [];
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
+        });
+
+        return expenses;
+      })
+      .then((expenses) => {
+        dispatch(setExpenses(expenses));
+      })
+  });
+};
+
 export {
   addExpense,
   startAddExpense,
   removeExpense,
-  editExpense
+  editExpense,
+  setExpenses,
+  startSetExpenses
 };
--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { startSetExpenses } from './actions/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -24,4 +22,9 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses())
+  .then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
