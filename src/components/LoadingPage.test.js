eff8f3d1430dbfa5f5e4db7f3ffc7764d2af5f6b
Ben Thompson <git@iambenthompson.com>
2018-01-02 20:05:15 -0500

    Adds proper LoadingPage, with looping gif; styles

---
+++ src/components/LoadingPage.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LoadingPage from './LoadingPage';
+
+test('should render LoadingPage correctly', () => {
+  const wrapper = shallow(<LoadingPage />);
+
+  expect(wrapper).toMatchSnapshot();
+});
